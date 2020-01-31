4741c8fc2c104dfb559994a937678db9c4e79a56
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2020-01-31 16:51:56 -0300

    Enviando os arquivos do projeto para o repositório no github

---
+++ app/src/main/java/com/bombapack/robotcontroller/ControllerActivity.java
@@ -0,0 +1,24 @@
+package com.bombapack.robotcontroller;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class ControllerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_controller);
+    }
+
+}
